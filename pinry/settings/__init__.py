cfe67976e4f5d0d2f26c7e73597a689c0c525fa8
Haoling <maya@fei-yen.jp>
2018-03-30 00:39:33 +0900

    pinry/settings/__init__.py

--- pinry/settings/__init__.py
+++ pinry/settings/__init__.py
@@ -3,20 +3,77 @@ import os
 from django.contrib.messages import constants as messages
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.join(os.path.realpath(os.path.dirname(__file__)), '../../')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'CHANGE-ME'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# Set to False to disable people from creating new accounts.
+ALLOW_NEW_REGISTRATIONS = True
 
-# SECURITY WARNING: use your actual domain name in production!
-ALLOWED_HOSTS = ['*']
+# Set to False to force users to login before seeing any pins. 
+PUBLIC = True
 
-# Application definition
 
-INSTALLED_APPS = [
+TIME_ZONE = 'America/New_York'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = '/data/media/'
+STATIC_ROOT = '/data/static/'
+TEMPLATE_DIRS = [os.path.join(SITE_ROOT, 'pinry/templates')]
+STATICFILES_DIRS = [os.path.join(SITE_ROOT, 'pinry/static')]
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
+)
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'pinry.users.middleware.Public',
+)
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'pinry.core.context_processors.template_settings',
+)
+AUTHENTICATION_BACKENDS = (
+    'pinry.users.auth.backends.CombinedAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+
+ROOT_URLCONF = 'pinry.urls'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+INTERNAL_IPS = ['127.0.0.1']
+MESSAGE_TAGS = {
+    messages.WARNING: 'alert alert-warning',
+    messages.ERROR: 'alert alert-danger',
+    messages.SUCCESS: 'alert alert-success',
+    messages.INFO: 'alert alert-info',
+}
+API_LIMIT_PER_PAGE = 50
+
+
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -28,125 +85,9 @@ INSTALLED_APPS = [
     'django_images',
     'pinry.core',
     'pinry.users',
-]
-
-ROOT_URLCONF = 'pinry.urls'
-
-MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pinry.users.middleware.Public',
-]
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'pinry/templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'pinry.core.context_processors.template_settings',
-            ],
-        },
-    },
-]
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-]
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pinry/static')]
-
-STATIC_ROOT = '/data/static/'
-
-MEDIA_ROOT = '/data/media/'
-
-WSGI_APPLICATION = 'pinry.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/data/production.db',
-    }
-}
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'America/New_York'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-
-# Set to False to disable people from creating new accounts.
-ALLOW_NEW_REGISTRATIONS = False
-
-# Set to False to force users to login before seeing any pins. 
-PUBLIC = True
-
-AUTHENTICATION_BACKENDS = [
-    'pinry.users.auth.backends.CombinedAuthBackend',
-    'django.contrib.auth.backends.ModelBackend',
-]
-
-LOGIN_URL = '/login/'
-
-LOGIN_REDIRECT_URL = '/'
-
-MESSAGE_TAGS = {
-    messages.WARNING: 'alert alert-warning',
-    messages.ERROR: 'alert alert-danger',
-    messages.SUCCESS: 'alert alert-success',
-    messages.INFO: 'alert alert-info',
-}
-
-API_LIMIT_PER_PAGE = 50
+)
 
 IMAGE_PATH = 'pinry.core.utils.upload_path'
-
 IMAGE_SIZES = {
     'thumbnail': {'size': [240, 0]},
     'standard': {'size': [600, 0]},
