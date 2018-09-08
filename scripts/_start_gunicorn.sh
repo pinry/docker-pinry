c06785db25f9a6f80899545e7acab488e011222a
winkidney <winkidney@gmail.com>
2018-09-08 04:54:47 -0700

    Refactor: Output log to stdout in docker
    So that we can view log using "docker logs <container-id>"

--- scripts/_start_gunicorn.sh
+++ scripts/_start_gunicorn.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 gunicorn pinry.wsgi -b 0.0.0.0:8000 -w 4 \
-    --capture-output --timeout 30 --user www-data --group www-data \
-    --log-file /var/log/gunicorn/error.log
+    --capture-output --timeout 30 --user www-data --group www-data
