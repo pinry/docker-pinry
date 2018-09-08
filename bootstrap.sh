a4f3fec1056cfa13852e7cc730c71ff72a19a1ad
winkidney <winkidney@gmail.com>
2018-09-08 05:25:56 -0700

    Feature: Bootstrap script will copy the docker-compose file

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ echo "Then you should manually edit your pinry/local_settings.py"
 echo "and replace SECRET_KEY with new secret-key if you had previously generated a"
 echo "pinry/local_settings.py."
 echo "If no previous pinry/local_settings.py generated, you can have a look and edit it."
+echo "If you want to use docker-compose, just edit docker-compose.yml and use 'docker-compose up'"
 
 SECRET_KEY=$(sudo docker run pinry/pinry /scripts/gen_key.sh)
 
@@ -16,6 +17,12 @@ echo "Your secret-key is(also saved/overwritten your pinry/production_secret_key
 echo ""
 echo ${SECRET_KEY}
 echo "=================================================================================="
+
+if [ ! -f ./pinry/local_settings.py ]
+then
+    cp ./pinry/local_settings.example.py ./pinry/local_settings.py
+    sed -i "s/secret\_key\_place\_holder/${SECRET_KEY}/" ./pinry/local_settings.py
+fi
 
 if [ ! -f ./pinry/local_settings.py ]
 then
